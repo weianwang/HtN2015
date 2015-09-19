54536718366a7854ba8d03c25da5d232e418b501
Marion X <marionxu1@gmail.com>
2015-09-19 16:34:06 -0400

    added editprofile template

--- client/profile.js
+++ client/profile.js
@@ -9,3 +9,34 @@ Template.profile.helpers({
 		return Profiles.find({userId: Meteor.userId()});
 	}
 });
+
+
+Template.editProfile.onCreated(function () {
+	this.subscribe('profiles');
+});
+
+Template.editProfile.helpers({
+	currentProfile: function() {
+		var user_id = Meteor.userId();
+		return Profiles.findOne({userId: user_id});
+	}
+});
+
+Template.editProfile.events({
+	"submit form": function (event) {
+		event.preventDefault();
+		var user_id = Meteor.userId();
+		var currProfile = Profiles.findOne({userId: user_id});
+
+		var firstname = $('[name="firstname"]').val();
+		var lastname = $('[name="lastname"]').val();
+
+		Profiles.update( { {userId: user_id}, $set: {
+			firstname: $('[name="firstname"]').val(),
+			lastname: $('[name="lastname"]').val(),
+			zipcode: $('[name="zipcode"]').val(),
+			bio: $('[name="bio"]').val()
+		}
+		});
+	}
+});
