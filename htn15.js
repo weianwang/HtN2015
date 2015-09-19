94b0eb6819850b24b9868ea9d9787736a84227e6
Weian Wang <weianwang95@gmail.com>
2015-09-19 13:51:45 -0400

    adding bootstrap for meteor

--- htn15.js
+++ htn15.js
@@ -1,16 +1,8 @@
 if (Meteor.isClient) {
-  Template.select.helpers({
-    'categories': function(){
-      // filler categories at the moment 
-      return ['Food', 'Dance', 'Music'];
-    }
-  });
-
-  Template.select.events({
-    'change #category-select': function(event, template){
-      var category = $(event.currentTarget).val();
-      console.log("category :" + category);
-      // just a filler for now, we will see what it takes later
+  Template.introduction.events({
+    'submit form': function(event){
+      event.preventDefault();
+      console.log('this works');
     }
   });
 }
