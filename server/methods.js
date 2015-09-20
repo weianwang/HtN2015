d19f080ed860d8430e753d8e3cc7fa88d6bcefe5
Marion X <marionxu1@gmail.com>
2015-09-19 22:50:57 -0400

    changed api key

--- server/methods.js
+++ server/methods.js
@@ -17,7 +17,7 @@ Meteor.methods({
 	//FOR API TESTING ATM
 	getZipcode: function() {
 		this.unblock();
-		var api_key = "js-iuOK5BlVWI8nDybp4WcTOga2Uz0GfrOxWCh1eCXbl5zYDKoHHABvRMyMlzsA1h8X";
+		var api_key = "irg6RlPQd2hCpkihXL0lish6cEHKTydcZtjRP8H2bYEAiXRgJSB1kT8xxPhrouDn";
 		return Meteor.http.call("GET", "https://www.zipcodeapi.com/rest/" + api_key + "/radius.json/48104/10/km");
 	}
 });
